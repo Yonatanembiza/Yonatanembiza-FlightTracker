915ff8b51395805e09baaf3b773b0e0587c25578
Yonatan <enyonathan@gmail.com>
2023-07-06 22:40:12 -0500

    This is a second commit

--- src/main/java/Application/Application.java
+++ src/main/java/Application/Application.java
@@ -10,27 +10,34 @@ import java.sql.SQLException;
 
 /**
  * There is no need to modify anything in this class.
- * The main method will start a new Javalin API on the console at localhost:8080.
- * Take a look at the FlightController class for API documentation as well as instructions for how to
+ * The main method will start a new Javalin API on the console at
+ * localhost:8080.
+ * Take a look at the FlightController class for API documentation as well as
+ * instructions for how to
  * access the API endpoints.
  */
 public class Application {
     /**
      * You can run this main method to run the API.
+     * 
      * @param args
      */
     public static void main(String[] args) {
         databaseSetup();
         FlightController flightController = new FlightController();
         Javalin app = flightController.startAPI();
-        app.start(8080);
+        app.start(8000);
     }
+
     /**
-     * For the purpose of this short exercise, this method will destroy and set up a new flight table.
-     * This is not a normal way to set up your tables, in real projects you should set up your database
-     * schema in a SQL editor such as DBeaver or DataGrip. Do not change anything in this method.
+     * For the purpose of this short exercise, this method will destroy and set up a
+     * new flight table.
+     * This is not a normal way to set up your tables, in real projects you should
+     * set up your database
+     * schema in a SQL editor such as DBeaver or DataGrip. Do not change anything in
+     * this method.
      */
-    public static void databaseSetup(){
+    public static void databaseSetup() {
         try {
             Connection conn = ConnectionUtil.getConnection();
             PreparedStatement ps1 = conn.prepareStatement("drop table if exists flight");
@@ -49,7 +56,7 @@ public class Application {
                     "('tampa', 'dallas')," +
                     "('dallas', 'tampa');");
             ps3.executeUpdate();
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
